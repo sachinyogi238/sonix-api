256e848dc7c53cb86c0dc92472a8eb5ea7d089f3
Sachin Yogi <sachinyogi238@protonmail.com>
2026-06-16 13:07:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from ytmusicapi import YTMusic
+from yt_dlp import YoutubeDL
 
 app = Flask(__name__)
 CORS(app)
@@ -16,3 +17,43 @@ def search():
     q = request.args.get("q", "")
     results = ytmusic.search(q, filter="songs")
     return jsonify(results[:20])
+
+@app.route("/audio")
+def get_audio():
+
+    video_id = request.args.get("videoId")
+
+    if not video_id:
+        return jsonify({
+            "error": "videoId required"
+        }), 400
+
+    try:
+
+        url = f"https://www.youtube.com/watch?v={video_id}"
+
+        ydl_opts = {
+            "format": "bestaudio",
+            "quiet": True,
+            "noplaylist": True
+        }
+
+        with YoutubeDL(ydl_opts) as ydl:
+
+            info = ydl.extract_info(
+                url,
+                download=False
+            )
+
+            return jsonify({
+                "audioUrl": info["url"]
+            })
+
+    except Exception as e:
+
+        return jsonify({
+            "error": str(e)
+        }), 500
+        
+if __name__ == "__main__":
+    app.run(debug=True)
